fba3322913791918127e5fa71ebb898636afa47d
Haihengly <your-email@example.com>
2026-09-07 15:58:33 +0700

    chore: stop tracking compiled dist output

--- api/dist/dto/diagram.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateDiagramDto {
-    id?: string;
-    name: string;
-    content: Record<string, unknown>;
-}
-export declare class UpdateDiagramDto {
-    name?: string;
-    content?: Record<string, unknown>;
-}
